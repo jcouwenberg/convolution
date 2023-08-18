bf27b0fb8c5aa6f875565a3ade358862bd964c66
jcouwenberg <jlcouwenberg@gmail.com>
2023-08-18 15:49:52 +1200

    testing

--- convolution2.c
+++ convolution2.c
@@ -1,53 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
-
-int main(int argc, char *argv[]) {
+#include <time.h>
+// comment
+int main(int argc, char *argv[])
+{
     FILE *file1, *file2, *file3;
     int i = 0;
     int filter[5][5];
-    int** data;
-    int** rlt;
+    int **data;
+    int **rlt;
     int j, k, l, m;
     int val;
     int iter;
 
-    if (argc < 5) {
-    	printf("Usage: ./convolution2 data1.txt filter1.txt temp111 1\n");
-    	return 1;
+    if (argc < 5)
+    {
+        printf("Usage: ./convolution2 data1.txt filter1.txt temp111 1\n");
+        return 1;
     }
     file1 = fopen(argv[1], "r");
     file2 = fopen(argv[2], "r");
-    if ((file1 == NULL) || (file2 == NULL)) {
-    	printf("Error: Files cannot open\n");
-    	return 1;
+    if ((file1 == NULL) || (file2 == NULL))
+    {
+        printf("Error: Files cannot open\n");
+        return 1;
     }
     iter = atoi(argv[4]);
-    if (iter == 0) {
-    	printf("Error: Invalid iteration\n");
-    	return 1;
-    } 
+    if (iter == 0)
+    {
+        printf("Error: Invalid iteration\n");
+        return 1;
+    }
     file3 = fopen(argv[3], "w");
-    data = (int**) malloc(sizeof(int*)*1024);
-    rlt = (int**) malloc(sizeof(int*)*1024);
-    for (i = 0; i < 1024; i++) {
-        data[i] = (int*) malloc(sizeof(int)*1024);
-        rlt[i] = (int*) malloc(sizeof(int)*1024);
+    data = (int **)malloc(sizeof(int *) * 1024);
+    rlt = (int **)malloc(sizeof(int *) * 1024);
+    for (i = 0; i < 1024; i++)
+    {
+        data[i] = (int *)malloc(sizeof(int) * 1024);
+        rlt[i] = (int *)malloc(sizeof(int) * 1024);
     }
 
     i = 0;
-    do {
+    do
+    {
         j = 0;
-        do {
-            if (j == 1023) {
+        do
+        {
+            if (j == 1023)
+            {
                 fscanf(file1, "%d\n", &val);
                 data[i][j] = val;
-//                printf("%d\n", data[i][j]);
+                //                printf("%d\n", data[i][j]);
             }
-            else {
+            else
+            {
                 fscanf(file1, "%d,", &val);
                 data[i][j] = val;
-//                printf("%d ", data[i][j]);
+                //                printf("%d ", data[i][j]);
             }
             j = j + 1;
         } while (j < 1024);
@@ -55,26 +64,29 @@ int main(int argc, char *argv[]) {
     } while (i < 1024);
 
     i = 0;
-    do {
+    do
+    {
         int j = 0;
-        do {
-            if (j == 4) {
+        do
+        {
+            if (j == 4)
+            {
                 fscanf(file2, "%d\n", &val);
                 filter[i][j] = val;
-//                printf("%d\n", filter[i][j]);
+                //                printf("%d\n", filter[i][j]);
             }
-            else {
+            else
+            {
                 fscanf(file2, "%d,", &val);
                 filter[i][j] = val;
-//               printf("%d ", filter[i][j]);
+                //               printf("%d ", filter[i][j]);
             }
             j = j + 1;
         } while (j < 5);
         i = i + 1;
     } while (i < 5);
 
-
-	int temp;
+    int temp;
     for (int x = 0; x < iter; x++)
     {
         for (i = 0; i < 1024; i++)
@@ -119,20 +131,22 @@ int main(int argc, char *argv[]) {
         }
     }
 
-
-    for (i = 0; i < 1024; i++) {
-        for (j = 0; j < 1024; j++) {
+    for (i = 0; i < 1024; i++)
+    {
+        for (j = 0; j < 1024; j++)
+        {
             fprintf(file3, "%d ", data[i][j]);
         }
         fprintf(file3, "\n");
     }
 
-    for (i = 0; i < 1024; i++) {
+    for (i = 0; i < 1024; i++)
+    {
         free(data[i]);
         free(rlt[i]);
     }
     free(data);
-	free(rlt);
+    free(rlt);
     fclose(file1);
     fclose(file2);
     fclose(file3);
